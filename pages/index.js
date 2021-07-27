f69f5a8f79b074ba52575bef6c0f7d2a368b5c5d
ecao77 <76830319+ecao77@users.noreply.github.com>
2021-07-26 20:26:42 -0500

    still trying to fix

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="webpage.js" className={styles.card}>
+          <a href="https://surviv.io" className={styles.card}>
             <h3>Other webpage &rarr;</h3>
             <p>Go to Goosy Webpage</p>
           </a>
