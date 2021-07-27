a5a9abd12d22d18e862e63670f79a0a018a5b0ad
ecao77 <76830319+ecao77@users.noreply.github.com>
2021-07-26 20:08:57 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/index.html" className={styles.card}>
+          <a href="pages/website.js" className={styles.card}>
             <h3>Other webpage &rarr;</h3>
             <p>Go to Goosy Webpage</p>
           </a>
