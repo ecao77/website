a7620dc7aa70d10fef759d1650f3e9463b269eda
ecao77 <76830319+ecao77@users.noreply.github.com>
2021-07-27 14:05:21 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Goosy! Goosy eats goose.
+          Homepage.
         </h1>
 
         <p className={styles.description}>
