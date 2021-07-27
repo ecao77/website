9a39bae5ae527876407a3b6728b5529995653b7e
ecao77 <76830319+ecao77@users.noreply.github.com>
2021-07-27 12:21:36 -0500

    Update page2.js

--- pages/page2.js
+++ pages/page2.js
@@ -16,8 +16,8 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          it is blog time
+          
         </p>
 
         <div className={styles.grid}>
