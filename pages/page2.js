3c01efa6f2eb090e6935f90b0485b7630634674e
ecao77 <76830319+ecao77@users.noreply.github.com>
2021-07-27 13:57:45 -0500

    Update page2.js

--- pages/page2.js
+++ pages/page2.js
@@ -19,7 +19,7 @@ export default function Home() {
           it is blog time
           
         </p>
-          <Link href="/index">
+          <Link href="/">
             <a>Go Back</a>
           </Link>
 
